81160953102bd48bb7d2da6f8eb410525d1bc014
Natthaphon kanththam <nptkkp.1048@gmail.com>
2024-09-16 00:01:12 +0700

    create sidebar responsive for mobile

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const toggleButton = document.getElementById("toggle-btn");
 const sidebar = document.getElementById("sidebar");
 
 function toggleSubMenu(button) {
+  if (!button.nextElementSibling.classList.contains("show")) {
+    closeAllSubMenus();
+  }
   //The nextElementSibling property returns the next element in the same tree level
   button.nextElementSibling.classList.toggle("show");
   button.classList.toggle("rotate");
@@ -16,6 +19,10 @@ function toggleSidebar() {
   sidebar.classList.toggle("close");
   toggleButton.classList.toggle("rotate");
 
+  closeAllSubMenus();
+}
+
+function closeAllSubMenus() {
   Array.from(sidebar.getElementsByClassName("show")).forEach((url) => {
     url.classList.remove("show");
     url.previousElementSibling.classList.remove("rotate");
